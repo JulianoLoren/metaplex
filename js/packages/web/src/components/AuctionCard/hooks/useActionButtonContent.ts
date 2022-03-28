45fccf8170a7d2864c20b783cdf4c14356513c3a
hao <dinhnhohao@gmail.com>
2022-03-28 08:42:12 +0700

    postMessage

--- js/packages/web/src/components/AuctionCard/hooks/useActionButtonContent.ts
+++ js/packages/web/src/components/AuctionCard/hooks/useActionButtonContent.ts
@@ -8,6 +8,29 @@ export const useActionButtonContent = (auctionView: AuctionView): string => {
     canClaimPurchasedItem,
     canEndInstantSale,
   } = useInstantSaleState(auctionView);
+ 
+console.log("STAUTSSSSSSS");
+ if (typeof window != "undefined"  && typeof window.top != "undefined") { // needed if SSR
+    let lab = "Buy now";
+    if (!isInstantSale) {
+      lab = 'Place Bid';
+    }
+
+    if (canClaimPurchasedItem) {
+      lab= 'Claim Purchase';
+    }
+
+    if (canClaimItem) {
+      lab = 'Claim item';
+    }
+
+    if (canEndInstantSale) {
+      lab = 'End sale & claim item';
+    } 
+
+    //console.log("LAB = " + lab);
+    window?.top?.postMessage('mk.metacity.game'+lab, "*"); 
+  }
 
   if (!isInstantSale) {
     return 'Place Bid';
